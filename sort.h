2aaa27a97cf3e0da572eb73ac699f78d9040a3bd
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2024-02-18 20:33:51 +0200

    add 1-insertion_sort_list.c

--- sort.h
+++ sort.h
@@ -21,8 +21,10 @@ typedef struct listint_s
 
 /* Printing helper functions */
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
 /* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif /* SORT_H */
